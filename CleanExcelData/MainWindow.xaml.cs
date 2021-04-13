d19c71d82e566da99d15103217f2208bc01ba2b8
zuizui <zuizui1990@outlook.com>
2021-04-13 13:59:45 +0800

    添加项目文件。

---
+++ CleanExcelData/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using ExcelHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CleanExcelData
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtFolder.Text))
+            {
+                var folderBase = txtFolder.Text;
+                if (Directory.Exists(folderBase))
+                {
+                    var clearnFolder = System.IO.Path.Combine(folderBase, "Clearned");
+                    if (!Directory.Exists(clearnFolder))
+                    {
+                        Directory.CreateDirectory(clearnFolder);
+                    }
+                    ExcelTool.Clean(folderBase, clearnFolder);
+                }
+
+            }
+        }
+    }
+}
---
+++ ExcelHelper/ExcelTool.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace ExcelHelper
+{
+    public class ExcelTool
+    {
+        private string _folderBase;
+        private string _clearnFolder;
+        public ExcelTool(string folderBase, string clearnFolder)
+        {
+            _folderBase = folderBase;
+            _clearnFolder = clearnFolder;
+        }
+        public void Clean()
+        {
+            var files = Directory.GetFiles(_folderBase);
+            foreach (var file in files)
+            {
+                TranslateFile(file);
+            }
+        }
+
+        private void TranslateFile(string file)
+        {
+            //NPOI.XSSF.UserModel.XSSFWorkbook
+        }
+    }
+}
