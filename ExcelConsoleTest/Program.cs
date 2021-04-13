6f5cc98a3b4d1bd0761d58400ea6bd140fe5f23e
plcly <shuipingplcly@gmail.com>
2021-04-14 00:15:34 +0800

    test parallel

--- ExcelConsoleTest/Program.cs
+++ ExcelConsoleTest/Program.cs
@@ -2,6 +2,7 @@
 using NPOI.XSSF.UserModel;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -17,32 +18,67 @@ namespace ExcelConsoleTest
             var sheet1 = wb.CreateSheet();
             var lastBlankRow = sheet1.CreateRow(21);
             var lastBlankCell = lastBlankRow.CreateCell(21, CellType.Blank);
-            Task.Run(() =>
-            {
-                Parallel.For(0, 20, i =>
-                {
-                    var row = sheet1.CreateRow(i);
-                    var cell = row.CreateCell(i);
-                    cell.SetCellValue("cell" + i);
-              });
-            }).Wait();
 
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            //ForeachCreate(sheet1);//51516
+            ParalleCreate(sheet1);//
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
             //var row1 = sheet1.CreateRow(0);
             //var cell2 = row1.CreateCell(1);
             //cell2.SetCellValue("Test");
             //var row3 = sheet1.CreateRow(2);
             //var cell5 = row3.CreateCell(4);
             //cell5.SetCellValue(5555);
-            if (File.Exists("Test.xlsx"))
+            var fileName = Path.Combine(Directory.GetCurrentDirectory(), "Test.xlsx");
+            if (File.Exists(fileName))
             {
-                File.Delete("Test.xlsx");
+                File.Delete(fileName);
             }
-            using (FileStream fs = File.OpenWrite("Test.xlsx")) 
+            using (FileStream fs = File.OpenWrite(fileName))
             {
                 wb.Write(fs);
             }
             Console.WriteLine("done");
             Console.Read();
+        }
+
+        private static void ParalleCreate(ISheet sheet1)
+        {
+            var listCell = new HashSet<ICell>();
+            for (int i = 0; i < 5000; i++)
+            {
+                var row = sheet1.CreateRow(i);
+                for (int j = 0; j <5000; j++)
+                {
+                    var cell=row.CreateCell(j);
+                    listCell.Add(cell);
+                    
+                }
+            }
+            Task.Run(() =>
+            {
+                Parallel.ForEach(listCell, cell =>
+                {
+                    cell.SetCellValue("TestCell");
+                });
+            }).Wait();
+            
+        }
+
+        private static void ForeachCreate(ISheet sheet1)
+        {
+            for (int i = 0; i < 5000; i++)
+            {
+                var row = sheet1.CreateRow(i);
+                for (int j = 0; j < 5000; j++)
+                {
+                    var cell = row.CreateCell(i);
+                    cell.SetCellValue("TestCell");
+                }
+            }
+            
         }
     }
 }
